15b48d4d91c2fd22dd036ae1f785ce41b1f6ca08
Anthony Bronkema <abronkema@customink.com>
2019-08-23 21:38:50 -0400

    adds temporary helper innerText, styling

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const mainContainer = document.querySelector(".container");
 
 function createGrid(number) {
-  for (i = 0; i < number; i++) {
+  for (i = 1; i <= number; i++) {
     const newBlock = document.createElement("div");
     let newDiv = mainContainer.appendChild(newBlock);
     newDiv.className = "block";
+    // TODO - Remove helpful innerText
+    newBlock.innerText = i;
   }
 }
